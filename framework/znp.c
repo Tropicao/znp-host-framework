e896e12850f9b72cf0ac5c00342b12808e3c4e52
Alexis Lothoré <alexis.lothore@gmail.com>
2018-01-13 13:41:26 +0100

    Add znp_strerr

--- framework/znp.c
+++ framework/znp.c
@@ -9,6 +9,59 @@
 //          * add typedef on cb
 //          * standard message structure to send
 
+
+typedef struct
+{
+    ZNPStatus status;
+    const char *string;
+}ZNPStatusString;
+
+static ZNPStatusString _status_list[] =
+{
+    {ZSuccess , "ZNP command success"},
+    {Zfailure , "ZNP failure"},
+    {ZinvalidParameter , "ZNP invalid parameter"},
+    {NV_ITEM_UNINIT, "ZNP NV item is not intialized"},
+    {NV_OPER_FAILED , "ZNP NV operation is not valid"},
+    {NV_BAD_ITEM_LEN , "ZNP NV item length is invalid"},
+    {ZmemError , "ZNP memory error"},
+    {ZbufferFull , "ZNP buffer is full"},
+    {ZunsupportedMode , "ZNP unsupported mode"},
+    {ZmacMemError , "ZNP MAC memory error"},
+    {zdoInvalidRequestType , "ZNP ZDO request is invalid"},
+    {zdoInvalidEndpoint , "ZNP ZDO request on invalid endpoint"},
+    {zdoUnsupported , "ZNP ZDO command is not supported"},
+    {zdoTimeout , "ZNP ZDO timeout"},
+    {zdoNoMatch , "ZNP ZDO found no match"},
+    {zdoTableFull , "ZNP ZDO table is full"},
+    {zdoNoBindentry , "ZNP ZDO found no bind entry"},
+    {ZsecNoKey , "ZNP found no security key"},
+    {ZsecMaxFrmCount , "ZNP security max frame count reached"},
+    {ZapsFail , "ZNP APS failed"},
+    {ZapsTableFull , "ZNP APS table is full"},
+    {ZapsIllegalRequest , "ZNP APS illegal request"},
+    {ZapsInvalidBinding , "ZNP APS binding is invalid"},
+    {ZapsUnsupportedAttrib , "ZNP APS unsupported attribute"},
+    {ZapsNotSupported , "ZNP APS operation not supported"},
+    {ZapsNoAck , "ZNP APS no acknowledgement received"},
+    {zApsDuplicateEntry , "ZNP APS duplicate entry"},
+    {ZapsNoBoundDevice , "ZNP APS no bound device found"},
+    {ZnwkInvalidParam , "ZNP APS invalid parameter"},
+    {ZnwkInvalidRequest , "ZNP APS invalid request"},
+    {ZnwkNotPermitted , "ZNP APS operation not permitted"},
+    {ZnwkStartupFailure , "ZNP NWK startup failure"},
+    {ZnwkTableFull , "ZNP NWK Table is full"},
+    {ZnwkUnknownDevice , "ZNP NWK unkown device"},
+    {ZnwkUnsupportedAttribute , "ZNP NWM unsupported attribute"},
+    {ZnwkNoNetworks , "ZNP NWK no network available"},
+    {ZnwkLeaveUnconfirmed , "ZNP NWK Leave command unconfirmed"},
+    {ZnwkNoAck , "ZNP NWK No Acknowledgement received"},
+    {ZnwkNoRoute , "ZNP NWK no route found"},
+    {ZMacNoAck , "ZNP MAC no acknowledgemeent received"}
+};
+
+static uint8_t _status_list_len = sizeof(_status_list)/sizeof(ZNPStatusString);
+
 typedef struct
 {
     int socket_fd;
@@ -74,5 +127,17 @@ int znp_message_cb_set(ZnpCallback_t cb __attribute__((unused)))
     // TODO : add first param as message type, second message as callback
     return 0;
 }
+
+const char *znp_strerror(ZNPStatus status)
+{
+    int index = 0;
+    for(index = 0; index < _status_list_len; index++)
+    {
+        if(_status_list[index].status == status)
+            return _status_list[index].string;
+    }
+    return NULL;
+}
+
 
 
--- framework/znp.h
+++ framework/znp.h
@@ -10,12 +10,57 @@
 
 typedef void (*ZnpCallback_t)(void);
 
+typedef enum
+{
+    ZSuccess =                  0x00,
+    Zfailure =                  0x01,
+    ZinvalidParameter =         0x02,
+    NV_ITEM_UNINIT =            0x09,
+    NV_OPER_FAILED =            0x0a,
+    NV_BAD_ITEM_LEN =           0x0c,
+    ZmemError =                 0x10,
+    ZbufferFull =               0x11,
+    ZunsupportedMode =          0x12,
+    ZmacMemError =              0x13,
+    zdoInvalidRequestType =     0x80,
+    zdoInvalidEndpoint =        0x82,
+    zdoUnsupported =            0x84,
+    zdoTimeout =                0x85,
+    zdoNoMatch =                0x86,
+    zdoTableFull =              0x87,
+    zdoNoBindentry =            0x88,
+    ZsecNoKey =                 0xa1,
+    ZsecMaxFrmCount =           0xa3,
+    ZapsFail =                  0xb1,
+    ZapsTableFull =             0xb2,
+    ZapsIllegalRequest =        0xb3,
+    ZapsInvalidBinding =        0xb4,
+    ZapsUnsupportedAttrib =     0xb5,
+    ZapsNotSupported =          0xb6,
+    ZapsNoAck =                 0xb7,
+    zApsDuplicateEntry =        0xb8,
+    ZapsNoBoundDevice =         0xb9,
+    ZnwkInvalidParam =          0xc1,
+    ZnwkInvalidRequest =        0xc2,
+    ZnwkNotPermitted =          0xc3,
+    ZnwkStartupFailure =        0xc4,
+    ZnwkTableFull =             0xc7,
+    ZnwkUnknownDevice =         0xc8,
+    ZnwkUnsupportedAttribute =  0xc9,
+    ZnwkNoNetworks =            0xca,
+    ZnwkLeaveUnconfirmed =      0xcb,
+    ZnwkNoAck =                 0xcc,
+    ZnwkNoRoute =               0xcd,
+    ZMacNoAck =                 0xe9,
+} ZNPStatus;
+
 
 int znp_init();
 void znp_shutdown();
 int znp_socket_get();
 void znp_loop_read();
 int znp_message_cb_set(ZnpCallback_t cb);
+const char *znp_strerror(ZNPStatus status);
 
 #endif
 
